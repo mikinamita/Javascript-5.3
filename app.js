3e6e5b861e2e324036560cb714fc219cbeea00bc
mikinamita <mikinamita@gmail.com>
2016-12-04 10:05:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//Assignment 5.3
+
+var employee = {name:"Namita", age:"30", salary:"5000",
+			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}};
+console.log("Employee Name " +employee.name);
+console.log("Employee Age " +employee.age);
+console.log("Employee Salary " +employee.salary);
+console.log("Employee City " +employee.address.city);
+console.log("Employee State " +employee.address.state);
+console.log("Employee PinCode " +employee.address.pin);
+
+
+	
